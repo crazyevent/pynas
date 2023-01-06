cbaf2941bb7d9d2c1df854787d52c711524fa36d
yongdaxie <xieyd1@chinatelecom.cn>
2023-01-06 17:36:43 +0800

    add public

--- pynas.py
+++ pynas.py
@@ -8,20 +8,30 @@ from gevent.pool import Pool
 from gevent.pywsgi import WSGIServer
 from gevent import monkey
 
+
 monkey.patch_all()
 config_file = './config.json'
 max_filename_len = 40
+
 
 def load_config(file_path):
     with open(file_path, 'r') as f:
         config = json.load(f)
         return config
 
+
 config = load_config(config_file)
 app = Bottle()
+public_path = os.path.split(os.path.realpath(__file__))[0] + os.seq + 'public'
+
+
+@app.route('/view/<filepath:path>')
+def web_views(filepath):
+    return bottle.static_file(filepath, view_path)
+
 
 @app.route('/<filepath:path>')
-def static_file(filepath):
+def share_public(filepath):
     sub_path = ''
     split_path = filepath.split('/')
     base_path = split_path[0]
@@ -59,8 +69,9 @@ def static_file(filepath):
         return html
     return 'not found',
 
+
 @app.route('/')
-def indexPath():
+def share_index():
     config = load_config(config_file)
     html = '<div style="width:1024px;margin:auto;">\n'
     left = '\t<div style="width:50%;float:left;padding=100px;">\n'
@@ -79,8 +90,7 @@ def indexPath():
     right += '\t</div>\n'
     html += left + right + '</div>\n'
     return html
-
-# bottle.run(host=config['addr'], port=config['port'], debug=True)
+    
 
 def run(addr, port):
     pool = Pool(256)
